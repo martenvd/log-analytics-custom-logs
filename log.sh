1ed6a82e42af0c19ecbca39e290522d039e21d40
Marten van Dijk <m.vandijk@ncontrol.cloud>
2020-04-15 15:20:50 +0200

    initial

--- log.sh
+++ log.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 export LANG=C.UTF-8
 
-wks_id='***********************'
-wks_shared_key='***********************'
+wks_id='******************************'
+wks_shared_key='*****************************'
 log_type='WebMonitorTest'
 
-json_data='{
-    "test": "test",
-    "test2": "test2"
-    }'
+file=$(cat $1)
 
 rfc1123date=$(TZ=GMT date '+%a, %d %b %Y %T %Z')
 x_headers="x-ms-date:$rfc1123date"
-content_length=${#json_data}
+content_length=${#file}
 full_string="POST\n$content_length\napplication/json\n$x_headers\n/api/logs"
-decoded_key=$(echo $wks_shared_key | base64 -d)
-encoded_hash=$(echo -en $full_string | openssl sha256 -mac HMAC -macopt key:$decoded_key -binary | base64)
+decoded_key=$(echo $wks_shared_key | base64 --decode | xxd -p -u -c 1000)
+encoded_hash=$(echo -en $full_string | openssl sha256 -mac HMAC -macopt hexkey:$decoded_key -binary | base64)
 
 authorization="SharedKey $wks_id:$encoded_hash"
 
@@ -27,4 +24,4 @@ header3="Log-Type: $log_type"
 header4="x-ms-date: $rfc1123date"
 header5="Content-Length: $content_length"
 
-curl -X POST "$uri" -d "$json_data" -H "$header1" -H "$header2" -H "$header3" -H "$header4" -H "$header5" --http1.1 
+curl -X POST "$uri" -d "$file" -H "$header1" -H "$header2" -H "$header3" -H "$header4" -H "$header5" --http1.1 
